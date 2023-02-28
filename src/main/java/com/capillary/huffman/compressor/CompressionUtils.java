3d02f2a16a5c1efb9be2f874f1095de8affcf7d0
sarthakj1904 <jainsarthak0419@gmail.com>
2023-02-28 17:59:05 +0530

    final working model

--- src/main/java/com/capillary/huffman/compressor/CompressionUtils.java
+++ src/main/java/com/capillary/huffman/compressor/CompressionUtils.java
@@ -3,10 +3,7 @@ package com.capillary.huffman.compressor;
 import com.capillary.huffman.mydefines.HuffmanData;
 import com.capillary.huffman.mydefines.Node;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CompressionUtils implements ICompressionUtils{
 
@@ -46,73 +43,219 @@ public class CompressionUtils implements ICompressionUtils{
     }
 
 
+ public static int getListLength(Map<String,String> lm){
+        int var=0;
+        for (Map.Entry<String,String> entry:lm.entrySet()){
+            var+=entry.getValue().length();
+        }
+        return (var+7)/8;
+ }
 
 
 
+    public HuffmanData createCompressedArray(String[] fileData, Map<String , String> lookupMap) {
 
+        int size=getListLength(lookupMap);
+        List<Byte> compressedArray=new ArrayList<>();
+
+        String str = "",curr="";
+        int idx=0,counter=0;
+
+        for (String s:fileData){
+            if (lookupMap.containsKey(s)){
+                str+=lookupMap.get(s);
+            }else {
+                for (char c:s.toCharArray()){
+                    str+=lookupMap.get(String.valueOf(c));
+                }
+            }
+
+
+//            if (idx==size-1 && str.length()<8){
+//                int j=0;
+//                for (j=0;j<str.length();j++){
+//                    if (str.charAt(j)=='0'){
+//                        counter++;
+//                    }
+//                }
+//            }
+            while (str.length()>=8){
+                curr=str.substring(0,8);
+                str=str.substring(8);
+                compressedArray.add((byte)Integer.parseInt(curr,2));
+
+            }
+        }
+        if (!str.isEmpty()){
+            while (str.length()>=8){
+                curr=str.substring(0,8);
+                str=str.substring(8);
+                compressedArray.add((byte)Integer.parseInt(curr,2));
+            }
+
+            if (str.length()<8){
+                int j=0;
+                for (j=0;j<str.length();j++){
+                    if (str.charAt(j)=='0'){
+                        counter++;
+                    }else break;
+                }
+                compressedArray.add((byte)Integer.parseInt(str,2));
+            }
+
+        }
+
+        Byte[] ccccc=new Byte[compressedArray.size()];
+        int iz=0;
+        for (Byte i:compressedArray){
+            ccccc[iz++]=i;
+
+        }
+//        System.out.println(Arrays.toString(ccccc));
+        return new HuffmanData(ccccc, (byte) counter);
+    }
 
 //
 //    01010101 0
-    public HuffmanData createCompressedArray(String[] fileData, Map<String , String> lookupMap) {
+    public HuffmanData c(String[] fileData, Map<String , String> lookupMap) {
         /////
+        int size=getListLength(lookupMap);
+        System.out.println(size);
+//        ArrayList<Byte> huffcodesList=new ArrayList<String>(size);
 
-        List<String> huffcodesList=new ArrayList<>();
+        Byte[] huffCodeBytes = new Byte[size];
+        String curr="",str="";
+
+
+
+
+        int it=0;
+
+
         StringBuilder temp=new StringBuilder();
+        int counter=0;
         for (String s:fileData){
             if(lookupMap.containsKey(s)){
-                temp.append(lookupMap.get(s));
-                ///
-                if (temp.length()==8){
-                    huffcodesList.add(temp.toString());
-                    temp.delete(0,temp.length());
-                }else if (temp.length()>8){
-                    huffcodesList.add(temp.substring(0,8));
-                    temp.delete(0,8);
-                }
+                str+=lookupMap.get(s);
+//                ///
+//                if (temp.length()>=8){
+//                    while (temp.length()>=8){
+//                        huffcodesList.add(temp.toString().substring(0,8));
+////
+//                        temp.delete(0,8);
+//                    }
+//                }
+
+
+
+
             }
             else{
                 for(char c: s.toCharArray()){
-                    temp.append(lookupMap.get(String.valueOf(c)));
+                    temp.append(lookupMap.get(Character.toString(c)));
                     //
-                    if (temp.length()==8){
-                        huffcodesList.add(temp.toString());
-                        temp.delete(0,temp.length());
-                    }else if (temp.length()>8){
-                        huffcodesList.add(temp.substring(0,8));
-                        temp.delete(0,8);
-//                        temp = new StringBuilder(temp.substring(8));
-                    }
+                    str+=lookupMap.get(String.valueOf(c));
+
+//                    if (temp.length()>=8){
+//                        while (temp.length()>=8){
+//                            huffcodesList.add(temp.toString().substring(0,8));
+////                            it++;
+//                            temp.delete(0,8);
+//                        }
+////                        temp = new StringBuilder(temp.substring(8));
+//                    }
 
                 }
             }
 
 
-        }
-        if (!temp.isEmpty())
-            huffcodesList.add(temp.toString());
 
-
-
-        int length=huffcodesList.size();
-//        Byte[] huffCodeBytes = new Byte[length];
-        Byte[] huffCodeBytes=new Byte[length];
-        int counter=0;
-        int idx = 0;
-
-        for (String x:huffcodesList){
-            if (x==huffcodesList.get(huffcodesList.size()-1)){
-                if (x.length()==8){
-                    huffCodeBytes[idx]=(byte) Integer.parseInt(x,2);
-                }else{
-                    for (int j=0;j<x.length();j++){
-                        if (x.charAt(j)=='0'){
+            if (it==size-1) {
+                if (str.length() <= 8) {
+                    int j = 0;
+                    for (j = 0; j < str.length(); j++) {
+                        if (str.charAt(j) == '0') {
                             counter++;
-                        }else break;
+                        } else {
+                            break;
+                        }
                     }
                 }
             }
-            huffCodeBytes[idx++]= (byte) Integer.parseInt(x,2);
+
+            while (str.length()>8) {
+
+
+                curr = str.substring(0, 8);
+                str = str.substring(8);
+                huffCodeBytes[it++] = (byte) Integer.parseInt(curr, 2);
+
+            }
         }
+        if (!str.isEmpty()){
+            if (it==size-1) {
+                if (str.length() <= 8) {
+                    int j = 0;
+                    for (j = 0; j < str.length(); j++) {
+                        if (str.charAt(j) == '0') {
+                            counter++;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+            while (str.length()>8){
+                curr=str.substring(0,8);
+                str=str.substring(8);
+                huffCodeBytes[it++]=(byte)Integer.parseInt(curr,2);
+            }
+        }
+
+
+        while (str.length()>8){
+            curr=str.substring(0,8);
+            str=str.substring(8);
+            huffCodeBytes[it++]=(byte)Integer.parseInt(curr,2);
+        }
+//        if (!temp.isEmpty()){
+//
+//            if (temp.length()>8){
+//                while (temp.length()>8){
+//                    huffcodesList.add(temp.toString().substring(0,8));
+//                    temp.delete(0,8);
+////                    it++;
+//                }
+//            }else{
+//                huffcodesList.add(temp.toString());
+////                it++;
+//            }
+//
+//        }
+//
+//
+//
+//
+//        int length=huffcodesList.size();
+////        Byte[] huffCodeBytes = new Byte[length];
+//        Byte[] huffCodeBytes=new Byte[length];
+//        int counter=0;
+//        int idx = 0;
+//
+//        for (String x:huffcodesList){
+//            if (x==huffcodesList.get(huffcodesList.size()-1)){
+//                if (x.length()==8){
+//                    huffCodeBytes[idx]=(byte) Integer.parseInt(x,2);
+//                }else{
+//                    for (int j=0;j<x.length();j++){
+//                        if (x.charAt(j)=='0'){
+//                            counter++;
+//                        }else break;
+//                    }
+//                }
+//            }
+//            huffCodeBytes[idx++]= (byte) Integer.parseInt(x,2);
+//        }
 
 
 
